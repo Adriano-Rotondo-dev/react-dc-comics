a6e3d66bd0230f5fa73f737a9cdfabc4a1ab4f35
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-07-14 18:45:25 +0200

    feat: restructure footer layout with columns and enhance styles for improved visual appeal

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,53 +2,92 @@ export default function Footer() {
   return (
     <footer>
       <div className="linkTabs">
-        <h4>COMICS</h4>
-        <ul className="comics footerUl">
-          <li>Characters</li>
-          <li>Comics</li>
-          <li>Movies</li>
-          <li>Tv</li>
-          <li>Games</li>
-          <li>Videos</li>
-          <li>New</li>
-        </ul>
-        <h4>SHOP</h4>
-        <ul className="shop footerUl">
-          <li>Shop DC</li>
-          <li>Shop DC Collectibles</li>
-        </ul>
-        <h4>DC</h4>
-        <ul className="DC footerUl">
-          <li>Terms of Use</li>
-          <li>Privacy Policy (New) </li>
-          <li>Ad Choices</li>
-          <li>Advetising</li>
-          <li>Jobs</li>
-          <li>Subscriptions</li>
-          <li>Talent Workshops</li>
-          <li>CPSC Certificates</li>
-          <li>Ratings</li>
-          <li>Shop Help</li>
-          <li>Contact Us</li>
-        </ul>
-        <h4>SITES</h4>
-        <ul className="sites footerUl">
-          <li>DC</li>
-          <li>MAD Magazine</li>
-          <li>DC Kids</li>
-          <li>DC Universe</li>
-          <li>DC Power Visa</li>
-        </ul>
+        <div className="container">
+          <div className="column">
+            <h4>DC COMICS</h4>
+            <ul className="comics footerUl">
+              <li>Characters</li>
+              <li>Comics</li>
+              <li>Movies</li>
+              <li>Tv</li>
+              <li>Games</li>
+              <li>Videos</li>
+              <li>New</li>
+            </ul>
+            <h4>SHOP</h4>
+            <ul className="shop footerUl">
+              <li>Shop DC</li>
+              <li>Shop DC Collectibles</li>
+            </ul>
+          </div>
+          <div className="column">
+            <h4>DC</h4>
+            <ul className="DC footerUl">
+              <li>Terms of Use</li>
+              <li>Privacy Policy (New) </li>
+              <li>Ad Choices</li>
+              <li>Advetising</li>
+              <li>Jobs</li>
+              <li>Subscriptions</li>
+              <li>Talent Workshops</li>
+              <li>CPSC Certificates</li>
+              <li>Ratings</li>
+              <li>Shop Help</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+          <div className="column">
+            <h4>SITES</h4>
+            <ul className="sites footerUl">
+              <li>DC</li>
+              <li>MAD Magazine</li>
+              <li>DC Kids</li>
+              <li>DC Universe</li>
+              <li>DC Power Visa</li>
+            </ul>
+          </div>
+        </div>
+        <img className="bgLogo" src="/img/dc-logo-bg.png" alt="dc-logo-bg" />
       </div>
       <div className="footerVisualLinks">
         <button className="signUp btn">SIGN UP NOW!</button>
         <div className="socials">
           <h3 className="follow">FOLLOW US</h3>
-          <a href="#" className="socialIcon"></a>
-          <a href="#" className="socialIcon"></a>
-          <a href="#" className="socialIcon"></a>
-          <a href="#" className="socialIcon"></a>
-          <a href="#" className="socialIcon"></a>
+          <a href="#" className="socialIcon">
+            <img
+              className="socialIcon"
+              src="/img/footer-facebook.png"
+              alt="facebook"
+            />
+          </a>
+          <a href="#" className="socialIcon">
+            <img
+              className="socialIcon"
+              src="/img/footer-twitter.png"
+              alt="twitter"
+            />
+          </a>
+          <a href="#" className="socialIcon">
+            <img
+              className="socialIcon"
+              src="/img/footer-youtube.png"
+              alt="youtube"
+            />
+          </a>
+          <a href="#" className="socialIcon">
+            <img
+              className="socialIcon"
+              src="/img/footer-pinterest.png"
+              alt="pinterest"
+            />
+          </a>
+          <a href="#" className="socialIcon">
+            <img
+              className="socialIcon"
+              src="/img/footer-periscope.png"
+              alt="periscope"
+            />
+          </a>
         </div>
       </div>
     </footer>
