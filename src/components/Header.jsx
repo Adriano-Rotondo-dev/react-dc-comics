e5e7fdac03604f843030871571e4160619ca8bb5
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-07-14 17:26:15 +0200

    refactor: restructure app components and remove unused styles; add header, main, and footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+export default function Header() {
+  return (
+    <header>
+      <img className="logo" src="/img/dc-logo.png" alt="dc-logo" />
+      <nav className="navbar">
+        <a className="" href="#">
+          CHARACTERS
+        </a>
+        <a className="" href="#">
+          COMICS
+        </a>
+        <a className="" href="#">
+          MOVIES
+        </a>
+        <a className="" href="#">
+          TV
+        </a>
+        <a className="" href="#">
+          GAMES
+        </a>
+        <a className="" href="#">
+          COLLECTIBLES
+        </a>
+        <a className="" href="#">
+          VIDEOS
+        </a>
+        <a className="" href="#">
+          FANS
+        </a>
+        <a className="" href="#">
+          NEWS
+        </a>
+        <a className="" href="#">
+          SHOP
+        </a>
+      </nav>
+    </header>
+  );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,31 @@
+export default function Main() {
+  return (
+    <main>
+      <div className="content">
+        <h3>Content goes here</h3>
+      </div>
+      <div className="visualLinks">
+        <a className="iconLink" href="#">
+          <img src="/img/buy-comics-digital-comics.png" alt="digital_comics" />
+          DIGITAL COMICS
+        </a>
+        <a className="iconLink" href="#">
+          <img src="/img/buy-comics-merchandise.png" alt="merch" /> DC
+          MERCHANDISE
+        </a>
+        <a className="iconLink" href="#">
+          <img src="/img/buy-comics-subscriptions.png" alt="subscriptions" />
+          SUBSCRIPTION
+        </a>
+        <a className="iconLink" href="#">
+          <img src="/img/buy-comics-shop-locator.png" alt="locator" /> COMIC
+          SHOP LOCATOR
+        </a>
+        <a className="iconLink" href="#">
+          <img src="/img/buy-dc-power-visa.svg" alt="visa" />
+          DC POWER VISA
+        </a>
+      </div>
+    </main>
+  );
+}
