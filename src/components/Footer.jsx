d8b82038a93c93f2e8580fc6622287e5bf416d15
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-07-14 17:59:24 +0200

    feat: enhance footer with link tabs and visual elements; update styles for improved layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,56 @@
 export default function Footer() {
-  return <footer> I AM FOOTER </footer>;
+  return (
+    <footer>
+      <div className="linkTabs">
+        <h4>COMICS</h4>
+        <ul className="comics footerUl">
+          <li>Characters</li>
+          <li>Comics</li>
+          <li>Movies</li>
+          <li>Tv</li>
+          <li>Games</li>
+          <li>Videos</li>
+          <li>New</li>
+        </ul>
+        <h4>SHOP</h4>
+        <ul className="shop footerUl">
+          <li>Shop DC</li>
+          <li>Shop DC Collectibles</li>
+        </ul>
+        <h4>DC</h4>
+        <ul className="DC footerUl">
+          <li>Terms of Use</li>
+          <li>Privacy Policy (New) </li>
+          <li>Ad Choices</li>
+          <li>Advetising</li>
+          <li>Jobs</li>
+          <li>Subscriptions</li>
+          <li>Talent Workshops</li>
+          <li>CPSC Certificates</li>
+          <li>Ratings</li>
+          <li>Shop Help</li>
+          <li>Contact Us</li>
+        </ul>
+        <h4>SITES</h4>
+        <ul className="sites footerUl">
+          <li>DC</li>
+          <li>MAD Magazine</li>
+          <li>DC Kids</li>
+          <li>DC Universe</li>
+          <li>DC Power Visa</li>
+        </ul>
+      </div>
+      <div className="footerVisualLinks">
+        <button className="signUp btn">SIGN UP NOW!</button>
+        <div className="socials">
+          <h3 className="follow">FOLLOW US</h3>
+          <a href="#" className="socialIcon"></a>
+          <a href="#" className="socialIcon"></a>
+          <a href="#" className="socialIcon"></a>
+          <a href="#" className="socialIcon"></a>
+          <a href="#" className="socialIcon"></a>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -26,41 +26,6 @@ export default function Main() {
           DC POWER VISA
         </a>
       </div>
-      <div className="linkTabs">
-        <ul className="comics">
-          <li>Characters</li>
-          <li>Comics</li>
-          <li>Movies</li>
-          <li>Tv</li>
-          <li>Games</li>
-          <li>Videos</li>
-          <li>New</li>
-        </ul>
-        <ul className="shop">
-          <li>Shop DC</li>
-          <li>Shop DC Collectibles</li>
-        </ul>
-        <ul className="DC">
-          <li>Terms of Use</li>
-          <li>Privacy Policy (New) </li>
-          <li>Ad Choices</li>
-          <li>Advetising</li>
-          <li>Jobs</li>
-          <li>Subscriptions</li>
-          <li>Talent Workshops</li>
-          <li>CPSC Certificates</li>
-          <li>Ratings</li>
-          <li>Shop Help</li>
-          <li>Contact Us</li>
-        </ul>
-        <ul className="sites">
-          <li>DC</li>
-          <li>MAD Magazine</li>
-          <li>DC Kids</li>
-          <li>DC Universe</li>
-          <li>DC Power Visa</li>
-        </ul>
-      </div>
     </main>
   );
 }
