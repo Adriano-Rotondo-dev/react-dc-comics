1ad0e2db683f039aeff442734c07ac8994aa7d32
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-07-16 13:01:32 +0200

    add component props for ComicsRows  import  in Main.jsx

--- src/components/Main Components/ComicsRows.jsx
+++ src/components/Main Components/ComicsRows.jsx
@@ -1,20 +1,14 @@
-import comics from "../../data/comics";
-
-export default function Comics() {
+export default function Comics({ comic }) {
   return (
-    <div className="content row">
-      {comics.map((comic) => (
-        <div className="col" key={comic.id}>
-          <div className="card">
-            <div className="cardHead">
-              <img src={comic.thumb} alt={comic.title} />
-            </div>
-            <div className="cardBody">
-              <h4>{comic.series}</h4>
-            </div>
-          </div>
+    <div className="col" key={comic.id}>
+      <div className="card">
+        <div className="cardHead">
+          <img src={comic.thumb} alt={comic.title} />
         </div>
-      ))}
+        <div className="cardBody">
+          <h4>{comic.series}</h4>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,14 @@
+import comics from "../data/comics";
 import Comics from "./Main Components/ComicsRows";
 
 export default function Main() {
   return (
     <main>
-      <Comics />
+      <div className="content row">
+        {comics.map((comic) => (
+          <Comics comic={comic} key={comic.id} />
+        ))}
+      </div>
       <button className="loadMore btn">LOAD MORE</button>
     </main>
   );
